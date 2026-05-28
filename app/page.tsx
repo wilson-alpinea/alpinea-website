bd437bfacd89a7eaa985909ed784e7866beafaeb
wilson-alpinea <wilson@alpinea.io>
2026-05-28 08:46:10 -0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-6xl font-light tracking-[0.2em]">
+        ALPINEA
+      </h1>
+    </main>
+  )
+}
