6abd08e292c819325c99d84f23f617d7d74f5cb7
wilson-alpinea <wilson@alpinea.io>
2026-07-31 18:11:29 -0300

    footer, goku, painel

--- app/lpfyslh1/ApprovalPanel.tsx
+++ app/lpfyslh1/ApprovalPanel.tsx
@@ -479,7 +479,7 @@ const DAY_6: DayContent = {
   date: "10 Mai",
   manha: {
     atracaoPrincipal: "Fushimi-Inari Taisha",
-    atracaoPrincipalImagem: "/images/dia6-fushimiinari-v2.png",
+    atracaoPrincipalImagem: "/images/dia6-fushimiinari-v3.png",
     pois: [],
   },
   tarde: {
